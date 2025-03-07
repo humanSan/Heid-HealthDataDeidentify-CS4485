4654fef4c5b7a5f0a6a1c66f514b99c2a32d9a5c
Cameron Kelly <cameron.kelly@utdallas.edu>
2025-03-06 22:13:08 -0600

    Added regex substitution for email and phone number

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import re
 with open("ehr JMS.txt", "r") as file:
     txt = file.read()
 
-    # substitute dates with regex
+    # substitute dates using regex
     # covers dates in the form 03/07/2025, 3-7-25, etc.
     txt = re.sub(r"\d{1,2}([\/-])\d{1,2}\1\d{2,4}", "*date*", txt)
+
+    # substitute emails using regex
+    txt = re.sub(r"[\w.%+-]+@[\w.]+\.[A-Za-z]+", "*email*", txt)
+
+    # substitute phone numbers using regex
+    # works only for 10-digit numbers with optional +1
+    txt = re.sub(r"(\+1\s*)?\(?(\d{3})\)?\s*-?\d{3}-?\s*\d{4}", "*phonenum*", txt)
 
     print(txt)
