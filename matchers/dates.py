d3bccaeef9b97ea88ea994b9c4db9c76f80dd02c
humanSan <48076233+humanSan@users.noreply.github.com>
2025-03-14 03:16:52 -0500

    dob2

--- matchers/dates.py
+++ matchers/dates.py
@@ -4,3 +4,7 @@ import re
 def strip_dates(text: str, two_way=False, sub="*date*"):
     # covers dates in the form 03/07/2025, 3-7-25, etc.
     return re.sub(r"\d{1,2}([\/-])\d{1,2}\1\d{2,4}", sub, text)
+
+def strip_dates(text: str, two_way=False, sub="*dob*"):
+    # covers dates in the form 03/07/2025, 3-7-25, etc.
+    return re.sub(r"\d{1,2}([\/-])\d{1,2}\1\d{2,4}", sub, text)
